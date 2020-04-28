98dbc5134fbb91d735871db351700190316f3a9b
veersen@17 <vishakha.naiknimbalkar@gmail.com>
2020-04-28 17:02:04 +0530

    Inital commit & create tenant functionality

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SaasProjectApp
+  class Application < Rails::Application
+     
+    # uncomment to ensure a common layout for devise forms
+    #   config.to_prepare do   # Devise
+    #     Devise::SessionsController.layout "sign"
+    #     Devise::RegistrationsController.layout "sign"
+    #     Devise::ConfirmationsController.layout "sign"
+    #     Devise::PasswordsController.layout "sign"
+    #   end   # Devise
+    # Initialize configuration defaults for originally generated Rails version.
+    #config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
